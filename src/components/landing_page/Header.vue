681a2186d9bb489b08e685a3269111c36aed8d72
Alex Darmawan <lekdarmawan99@gmail.com>
2023-06-02 21:52:38 +0700

    new config

---
+++ src/components/landing_page/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="row text-white">
+      <div class="col-md-6">
+        <p class="mt-3"><b-icon-telephone> </b-icon-telephone> +62 812 3456 7890 <span class="ml-3"><b-icon-envelope > </b-icon-envelope > help@militaryinforce.com</span></p>
+      </div>
+      <div class="col-md-6 text-right">
+        <p class="mt-3"><b-icon-geo-alt> </b-icon-geo-alt> Jl. Anggrek No. 12 Jebres, Surakarta, Jawa Tengah</p>
+      </div>
+      <div class="col-md-12 m-none">
+        <hr class="bg-secondary m-0">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'Header'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/landing_page/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="home">
+    <Header />
+    <Navbar />
+    <section>
+      <div class="container">
+      <div class="row mt-4">
+        <div class="col-md-5">
+          <div class="d-flex h-100 text-white" style="height:100px;">
+            <div class="justify-content-center align-self-center">
+              <h5>SELAMAT DATANG DI</h5><hr>
+              <h1>MILITARY IN FORCES LEARNING CENTRE</h1>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-7">
+          <div>
+            <b-carousel
+              id="carousel-fade"
+              style="text-shadow: 0px 0px 2px #000"
+              fade
+              indicators
+              img-width="1024"
+              img-height="720"
+            >
+              <b-carousel-slide
+                caption="First Slide"
+                img-src="https://picsum.photos/1024/480/?image=10"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                caption="Second Slide"
+                img-src="https://picsum.photos/1024/480/?image=12"
+              ></b-carousel-slide>
+              <b-carousel-slide
+                caption="Third Slide"
+                img-src="https://picsum.photos/1024/480/?image=22"
+              ></b-carousel-slide>
+            </b-carousel>
+          </div>
+        </div>
+      </div>
+    </div>
+    </section>
+    
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/landing_page/Header.vue'
+import Navbar from '@/components/landing_page/Navbar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Navbar
+  }
+}
+</script>
