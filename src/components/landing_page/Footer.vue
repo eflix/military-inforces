895bf4c087be48fa0ce59b375a824d831176b650
Alex Darmawan <lekdarmawan99@gmail.com>
2023-06-04 03:38:17 +0700

    add new file footer

---
+++ src/components/landing_page/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mt-4">
+                <img src="../../assets/images/logo.png" alt="logo" width="40%" style="">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum excepturi cupiditate inventore, at explicabo atque quae aperiam impedit eum ipsa voluptate doloremque saepe obcaecati iure velit exercitationem ullam nisi sint!</p>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col-md-2"></div> -->
+            <div class="col-md-2 mt-4">
+                <h5>SITEMAP</h5>
+                <p class="">Beranda</p><br>
+                <p class="">Sekolah Kedinasan</p><br>
+                <p class="">Tentang Kami</p><br>
+            </div>
+            <div class="col-md-4 mt-4">
+                <h5>INFORMASI KONTAK</h5>
+                <p class="">Military In Forces Learning Centre</p><br>
+                <p class="">09875744848</p><br>
+                <p class="">help@militaryinforces.com</p><br>
+                <p class="">Jl. Anggrek No. 12 Jebres, Surakarta, Jawa Tengah</p><br>
+            </div>
+        </div>
+    </div>
+</div>
+  
+</template>
+
+<script>
+export default {
+    name : 'Footer'
+}
+</script>
+
+<style>
+
+</style>
