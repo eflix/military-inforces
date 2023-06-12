c13dccd2273bf3f6e97ffa8c0d327059c994e74a
Alex Darmawan <lekdarmawan99@gmail.com>
2023-06-12 07:04:53 +0700

    setting dashboard

--- src/main.js
+++ src/main.js
@@ -8,6 +8,18 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/main.css'
 
+// vue dashboard
+import VueDashboard from 'vue-dashboard-vd';
+Vue.use(VueDashboard);
+
+import HeaderItems from './components/HeaderItems.vue';
+import SidebarHeader from './components/SidebarHeader.vue';
+import SidebarItems from './components/SidebarItems.vue';
+
+Vue.component('header-items', HeaderItems);
+Vue.component('sidebar-items', SidebarItems);
+Vue.component('sidebar-header', SidebarHeader);
+
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,63 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/landing_page/Home.vue'
-import SekolahKedinasan from '../views/landing_page/SekolahKedinasan.vue'
-import FotoKegiatan from '../views/landing_page/FotoKegiatan.vue'
-import DaftarTentor from '../views/landing_page/DaftarTentor.vue'
+// import Home from '../views/landing_page/Home.vue'
+// import SekolahKedinasan from '../views/landing_page/SekolahKedinasan.vue'
+// import FotoKegiatan from '../views/landing_page/FotoKegiatan.vue'
+// import DaftarTentor from '../views/landing_page/DaftarTentor.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+
   {
     path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/sekolah_kedinasan',
-    name: 'sekolah_kedinasan',
-    component: SekolahKedinasan
-  },
-  {
-    path: '/foto_kegiatan',
-    name: 'foto_kegiatan',
-    component: FotoKegiatan
-  },
-  {
-    path: '/daftar_tentor',
-    name: 'daftar_tentor',
-    component: DaftarTentor
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    name: 'About',
+    component: () => import('../views/About.vue'),
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: () => import('../views/Account.vue'),
+  },
+  {
+    path: '/statistics',
+    name: 'Statistics',
+    component: () => import('../views/Statistics.vue'),
+  },
+
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/sekolah_kedinasan',
+  //   name: 'sekolah_kedinasan',
+  //   component: SekolahKedinasan
+  // },
+  // {
+  //   path: '/foto_kegiatan',
+  //   name: 'foto_kegiatan',
+  //   component: FotoKegiatan
+  // },
+  // {
+  //   path: '/daftar_tentor',
+  //   name: 'daftar_tentor',
+  //   component: DaftarTentor
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = new VueRouter({
