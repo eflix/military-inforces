d22b57cf2902920af1114d893d7846f3b2cab234
Alex Darmawan <lekdarmawan99@gmail.com>
2023-07-10 17:09:57 +0700

    history ujian

---
+++ src/Member/views/DetailHistory.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container mt-3">
+    <h3>Detail Riwayat Ujian</h3>
+    <!-- <b-table
+      id="table-transition-example"
+      :items="products.data"
+      :fields="fields"
+      striped
+      small
+      primary-key="nama"
+      :tbody-transition-props="transProps"
+    ></b-table> -->
+    {{$route.params.UjianId}}
+
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+// console.log(this.$route.params.UjianId)
+
+export default {
+
+data(){
+    return{
+      username: this.$session.get("username")?this.$session.get("username"):"",
+      products:[],
+      transProps: {
+          // Transition name
+          name: 'flip-list'
+        },
+        fields: [
+        {key : "nama", sortable: true},
+        {key : "jabatan", sortable: true}
+        ]
+    }
+  },
+  methods:{
+     setProducts(data){
+      this.products = data
+    }
+  },
+  mounted(){
+    axios.get('http://localhost/api2/military_inforces/member/member/tentor', {
+      headers: {
+        "Content-type": "application/json",
+        }
+    })
+  .then( (response) =>this.setProducts(response.data))
+  .catch((error) => console.log(error));
+  }
+}
+</script>
+
+<style>
+.card-header {
+    font-size: 2rem;
+}
+</style>
+
