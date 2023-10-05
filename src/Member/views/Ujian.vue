5458782780392fd68409c17eecc671e91e9968b1
Alex Darmawan <lekdarmawan99@gmail.com>
2023-10-05 15:46:25 +0700

    fix ujian

--- src/Member/views/Ujian.vue
+++ src/Member/views/Ujian.vue
@@ -30,7 +30,7 @@
           <div class="tombol">
             <button class="btn btn-warning" v-if="parseInt(no_urut)>1" v-on:click.prevent="getPertanyaanByNo(parseInt(no_urut)-1,parseInt(id_ujian))">Sebelumnya</button>
             <button class="btn btn-success" v-if="parseInt(no_urut)<parseInt(total_number)" v-on:click.prevent="getPertanyaanByNo(parseInt(no_urut)+1,parseInt(id_ujian))">Selanjutnya</button>
-            <button class="btn btn-success" v-if="answer && parseInt(no_urut)<parseInt(total_number)" v-on:click.prevent="saveNext(parseInt(id_ujian),parseInt(Pertanyaan.data[0]['id']),answer,parseInt(no_urut))">Simpan & Selanjutnya</button>
+            <button class="btn btn-success" v-if="answer && parseInt(no_urut)<parseInt(total_number)" v-on:click.prevent="saveNext(parseInt(id_ujian),parseInt(id_pertanyaan),answer,parseInt(no_urut))">Simpan & Selanjutnya</button>
             <button class="btn btn-success" v-if="answer && parseInt(no_urut)>=parseInt(total_number)" v-on:click.prevent="saveFinish(parseInt(id_ujian))">Simpan & Selesai</button>
           </div>
         </b-col>
@@ -53,6 +53,7 @@ export default {
   name: 'Ujian',
   data() {
     return {
+      id_user: this.$session.get("id")?this.$session.get("id"):"",
       countDownToTime : new Date("October 03, 2023 03:50:00").getTime(),
       timerOutput:  null,
       answer : "",
@@ -71,6 +72,7 @@ export default {
       last_number:0,
       id_ujian:0,
       total_number:0,
+      id_pertanyaan:0,
     }
 },
 methods: {
@@ -100,6 +102,7 @@ methods: {
     getPertanyaanByNo(no,id_ujian){
       
       axios.post('https://bimbel-militaryinforces.com/api/member/member/pertanyaanByNo', {
+              id_user : this.id_user,
               no : no,
               id_ujian : id_ujian,
             }, {
@@ -110,12 +113,14 @@ methods: {
       .then( 
         (response) => {
           this.setPertanyaan(response.data)
+          console.log(response.data);
           this.no_urut = response.data.data[0]['no_urut']
           this.text_pertanyaan = response.data.data[0]['text_pertanyaan']
           this.a = response.data.data[0]['a']
           this.b = response.data.data[0]['b']
           this.c = response.data.data[0]['c']
           this.d = response.data.data[0]['d']
+          this.id_pertanyaan = response.data.data[0]['id']
           if(response.data.data[0]['jawaban']){
             this.answer = response.data.data[0]['jawaban']
           } else {
@@ -127,6 +132,7 @@ methods: {
     },
     saveNext(id_ujian,id_pertanyaan,answer,no){
       axios.post('https://bimbel-militaryinforces.com/api/member/member/save_answer', {
+              id_user : this.id_user,
               id_ujian : id_ujian,
               id_pertanyaan : id_pertanyaan,
               answer : answer,
@@ -137,8 +143,11 @@ methods: {
             },
         })
       .then( 
-        this.getPertanyaanByNo(no+1,id_ujian),
-        window.location.reload()
+        (response) => {
+          // console.log(response.data)
+          this.getPertanyaanByNo(no+1,id_ujian),
+          window.location.reload()
+        }
       )
       .catch((error) => console.log(error));
     },
@@ -162,30 +171,32 @@ methods: {
   },
   mounted() {
        setInterval(() => { this.startTimer() }, 1000);
-       axios.get('https://bimbel-militaryinforces.com/api/member/member/temp_detail_ujian', {
+       axios.post('https://bimbel-militaryinforces.com/api/member/member/temp_detail_ujian', 
+       {id_user:this.id_user},
+       {
       headers: {
-        "Content-type": "application/json",
-        }
+        "Content-type": "text/plain",
+      }
     })
   .then((response) => {
+    // console.log(response.data);
     this.setTempUjian(response.data)
-    console.log(response.data)
     var date = new Date(response.data.data[0]['tanggal_ujian'] + ' ' + response.data.data[0]['finish_time'])
     this.countDownToTime = date.getTime()
     this.last_number = response.data.data[0]['last_number']
     this.total_number = response.data.data[0]['total_number']
     this.id_ujian = response.data.data[0]['id']
     axios.post('https://bimbel-militaryinforces.com/api/member/member/pertanyaanByNo', 
-    {no : this.last_number,id_ujian : this.id_ujian},{
+    {id_user:this.id_user,no : this.last_number,id_ujian : this.id_ujian},{
           headers: {
             "Content-type": "text/plain",
-            },
+          },
         },
         
         )
       .then( 
         (response) => {
-          // console.log(response.data);
+          console.log(response.data);
           this.setPertanyaan(response.data)
           this.no_urut = response.data.data[0]['no_urut']
           this.text_pertanyaan = response.data.data[0]['text_pertanyaan']
@@ -193,6 +204,7 @@ methods: {
           this.b = response.data.data[0]['b']
           this.c = response.data.data[0]['c']
           this.d = response.data.data[0]['d']
+          this.id_pertanyaan = response.data.data[0]['id']
           if(response.data.data[0]['jawaban']){
             this.answer = response.data.data[0]['jawaban']
           } else {
